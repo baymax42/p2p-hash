52f2df4100e49299cbcc713c3675d34cd5158fa8
Paweł Górski <dev.baymax42@gmail.com>
2018-12-25 21:40:06 +0100

    Changed names in docker-compose and adjusted utils package index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,6 @@
-import { Console } from 'console'
-import process from 'process'
-
 export const CHARS = 'abcdefghijklmnopqrstuvwxyz'
 export const NUMBERS = '0123456789'
 export const SPECIALS = '!@#$%^&*()_+{}[]-=;\':"|\\<>,.?/~`'
 export const ALL_SYMBOLS = (CHARS + CHARS.toLocaleUpperCase() + NUMBERS + SPECIALS)
-export const LOGGER = new Console({ stdout: process.stdout, stderr: process.stderr })
+export { ActionManager } from './ActionManager'
+export { Logger, LOGGER } from './Logger'
