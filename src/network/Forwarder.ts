41ecc2555e5f49ff8b087de1b410a139b90bc183
Paweł Górski <dev.baymax42@gmail.com>
2018-12-25 21:40:46 +0100

    Removed implementation of Registry as it might be integrated directly (and probably changed) in Facade.

--- src/network/Forwarder.ts
+++ src/network/Forwarder.ts
@@ -1,32 +1,26 @@
 import dgram, { Socket } from 'dgram'
 import { LOGGER } from 'utils'
-import { IRegistry } from './Registry'
 
 // IPC class for sending messages
 export class Forwarder {
   private client: Socket
-  private registry: IRegistry
 
-  constructor (reg: IRegistry) {
+  constructor () {
     // Create socket and enable broadcast flag
     this.client = dgram.createSocket('udp4')
     // We need to bind socket to random port and then set the flag
     this.client.bind(0, () => {
       this.client.setBroadcast(true)
     })
-    this.registry = reg
   }
 
   // Serialize and send message
-  public forwardMessage (id: string, port: number, message: any): void {
-    const ip = this.registry.getIP(id)
-    if (ip) {
-      this.client.send(this.marshal(message), port, ip, (err) => {
-        if (err != null) {
-          LOGGER.error(err)
-        }
-      })
-    }
+  public forwardMessage (address: string, port: number, message: any): void {
+    this.client.send(this.marshal(message), port, address, (err) => {
+      if (err != null) {
+        LOGGER.error(err)
+      }
+    })
   }
 
   // Serialize message to format accepted by IPC method
