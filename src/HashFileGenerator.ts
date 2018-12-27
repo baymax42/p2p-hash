b1dfea7d161fd8dcd830b7b8d8f757ff6c6e4d51
Ada Sozańska <ada.sozanska@gmail.com>
2018-12-27 12:45:57 +0100

    added multiple hash method selection to the file generator

--- src/HashFileGenerator.ts
+++ src/HashFileGenerator.ts
@@ -1,7 +1,7 @@
 import * as crypto from 'crypto'
 import { RandomString } from './generator'
 
-function createHashFile (amount: number, wordLength: number, hashMethod: string, path: string) {
+function createHashFile (amount: number, wordLength: number, hashMethods: string[], path: string) {
   const randomStringIterator = new RandomString(wordLength, amount).iterator()
   const fs = require('fs')
   const logger = fs.createWriteStream(path, {
@@ -10,6 +10,7 @@ function createHashFile (amount: number, wordLength: number, hashMethod: string,
 
   while (randomStringIterator.hasNext()) {
     const randomString = randomStringIterator.next()
+    const hashMethod = hashMethods[Math.floor(Math.random() * hashMethods.length)]
     const hash = crypto.createHash(hashMethod)
     const hashString = hash.update(randomString).digest('hex')
     logger.write(hashString + '\n')
@@ -17,4 +18,5 @@ function createHashFile (amount: number, wordLength: number, hashMethod: string,
   logger.end()
 }
 
-createHashFile(5, 4, 'md5', 'testHashFile.txt')
+const methods = ['md5', 'sha1']
+createHashFile(5, 4, methods, 'testHashFile.txt')
