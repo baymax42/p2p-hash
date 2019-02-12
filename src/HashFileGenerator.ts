2c7a8f5ba0dc74671e7aa7cadb01ea3a271cd180
Paweł Górski <dev.baymax42@gmail.com>
2019-02-12 21:12:20 +0100

    Added third hash method and added passing parameters when launching script.

--- src/HashFileGenerator.ts
+++ src/HashFileGenerator.ts
@@ -1,5 +1,6 @@
-import * as crypto from 'crypto'
+import { createHash } from 'crypto'
 import { RandomString } from './generator'
+import { LOGGER } from './utils'
 
 function createHashFile (amount: number, wordLength: number, hashMethods: string[], path: string) {
   const randomStringIterator = new RandomString(wordLength, amount).iterator()
@@ -10,12 +11,29 @@ function createHashFile (amount: number, wordLength: number, hashMethods: string
   while (randomStringIterator.hasNext()) {
     const randomString = randomStringIterator.next()
     const hashMethod = hashMethods[Math.floor(Math.random() * hashMethods.length)]
-    const hash = crypto.createHash(hashMethod)
+    const hash = createHash(hashMethod)
     const hashString = hash.update(randomString).digest('hex')
     logger.write(hashMethod + ' ' + hashString + '\n')
   }
   logger.end()
 }
 
-const methods = ['md5', 'sha1']
-createHashFile(10, 4, methods, 'testHashFile.txt')
+const methods = ['md5', 'sha1', 'sha256']
+
+let hashAmount = -1
+let length = -1
+process.argv.forEach((value, index) => {
+  const arg = value.split('=')
+  if (arg.length === 2 && arg[0] === 'amount') {
+    hashAmount = Number(arg[1])
+  } else if (arg.length === 2 && arg[0] === 'length') {
+    length = Number(arg[1])
+  }
+})
+
+if (hashAmount < 1 || length < 1) {
+  LOGGER.log('Example usage:')
+  LOGGER.log('node fileGenerator.js amount=2 length=5')
+} else {
+  createHashFile(hashAmount, length, methods, 'testHashFile.txt')
+}
