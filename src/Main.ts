6c591ec15320d0664266cb787d012146d8b9a711
Paweł Górski <dev.baymax42@gmail.com>
2019-02-13 00:39:40 +0100

    Draft index.html for presentation and Main file with forking Server process.

--- src/Main.ts
+++ src/Main.ts
@@ -1,14 +1,11 @@
+import { fork } from 'child_process'
 import * as fs from 'fs'
+import * as path from 'path'
 import { NetworkRegister } from './network/NetworkRegister'
 import { Peer } from './state'
 import { ActionManager, HashManager, IHashEntry } from './utils'
 
-// const serverProcess = fork(
-//   path.resolve('server.js'),
-//   [],
-//   {
-//     stdio: ['pipe', 'pipe', 'pipe', 'ipc']
-//   })
+const serverProcess = fork(path.resolve('server.js'), [])
 
 // const worker = fork(
 //   path.resolve('worker.js'),
@@ -39,9 +36,32 @@ const hashManager = new HashManager()
 if (hashFile) {
   fs.readFile(hashFile, (err, data) => {
     if (!err) {
-      const hashes = parseFile(data.toString())
-      hashManager.hashes = hashes
+      hashManager.hashes = parseFile(data.toString())
+    } else {
+      throw ReferenceError('File does not exist')
     }
   })
 }
+
+serverProcess.on('message', (content) => {
+  hashManager.once('change', (out) => {
+    serverProcess.send({
+      content: {
+        all: hashManager.hashes,
+        solved: hashManager.hashes.filter((v) => v.plaintext !== '')
+      },
+      type: 'message'
+    })
+  })
+})
+
+// TODO: remove after it is fully functional
+setInterval(() => {
+  hashManager.update({
+    hash: 'f7b013eeb82da1340028f188231e6645535c5bee1ccbdf8ce5476fc1cf327eb9',
+    method: 'sha256',
+    plaintext: 'abcd'
+  })
+}, 4000)
+
 const peer = new Peer(hashManager, new NetworkRegister(), new ActionManager(), 'newbie')
--- src/Server.ts
+++ src/Server.ts
@@ -6,8 +6,35 @@ const app = express()
 const port = 8000
 const logger = new Console({ stdout: process.stdout, stderr: process.stderr })
 
-// app.use('/', express.static('static'))
+async function getSolved () {
+  if (process != null) {
+    // @ts-ignore
+    process.send('getHashes')
+    return new Promise((resolve, reject) => {
+      process.once('message', (result: any) => {
+        if (result.type === 'message' && result.content) {
+          resolve(result.content)
+        } else {
+          reject()
+        }
+      })
+    })
+  } else {
+    throw ReferenceError('This process should be spawned as child process')
+  }
+}
+
+app.use('/', express.static('../static'))
 app.get('/', (req, res) => {
-  res.send('swaef')
+  res.sendFile('index.html')
 })
+
+app.post('/api/hashes', (req, res) => {
+  getSolved().then((value) => {
+    res.send(JSON.stringify(value))
+  }).catch(() => {
+    res.sendStatus(500)
+  })
+})
+
 app.listen(port, () => logger.log(`Example app listening on port ${port}!`))
